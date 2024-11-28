8bb4d9675213bd5b9d155948ebe5af2704303d84
williamlambert19 <165288638+williamlambert19@users.noreply.github.com>
2024-11-28 11:17:18 +0000

    Update web.py

--- web.py
+++ web.py
@@ -3,8 +3,38 @@ import pandas as pd
 import plotly.express as px
 import requests
 import json
-import hashlib
+import hmac
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+print(os.getenv('PASSWORD'))
+
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], os.getenv('PASSWORD')):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
 
 data = json.load(open('editorial-topics.json'))
 broad_concepts = {}
